3fc8952de56cb540c89c280682daa40b9a62e3c1
Kamil Popardowski <kamil.popardowski@protonmail.com>
2025-11-21 16:30:19 -0600

    Make a badge that will pop out when google maps is open

---
+++ background.js
@@ -0,0 +1,60 @@
+const mapsRegex = /^https?:\/\/(www\.)?google\.[^/]+\/maps/i;
+
+async function updateBadge(tabId, url) {
+  if (!url) return;
+  const isMaps = mapsRegex.test(url);
+
+  try {
+    console.log('[merge-stops] updateBadge', { tabId, url, isMaps });
+    if (isMaps) {
+      await chrome.action.setBadgeText({ tabId, text: 'GO!' });
+      await chrome.action.setBadgeBackgroundColor({ tabId, color: '#ec4899' });
+      await chrome.action.setTitle({
+        tabId,
+        title: 'Maps detected. Click to merge all stops.',
+      });
+    } else {
+      await chrome.action.setBadgeText({ tabId, text: '' });
+      await chrome.action.setTitle({ tabId, title: '' });
+    }
+  } catch (err) {
+    console.error('[merge-stops] badge update failed', err);
+  }
+}
+
+console.log('[merge-stops] background worker starting');
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('[merge-stops] onInstalled');
+  chrome.action.setBadgeText({ text: '' }).catch(() => {});
+});
+
+chrome.runtime.onStartup.addListener(() => {
+  console.log('[merge-stops] onStartup');
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' || changeInfo.url) {
+    const url = changeInfo.url ?? tab.url;
+    if (url) updateBadge(tabId, url);
+  }
+});
+
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  chrome.tabs
+    .get(tabId)
+    .then((tab) => {
+      if (tab?.url) updateBadge(tabId, tab.url);
+    })
+    .catch(() => {
+      /* ignore */
+    });
+});
+
+chrome.tabs.query({}).then((tabs) => {
+  tabs.forEach((tab) => {
+    if (tab.id !== undefined && tab.url) {
+      updateBadge(tab.id, tab.url);
+    }
+  });
+});
